7198d05d3d6b4562407ed78ed4c748a04c00dadf
pbenzoni <Asimov17!git>
2021-09-27 13:11:20 -0400

    Fixing range facets for firefox

--- src/components/RangeFacet.tsx
+++ src/components/RangeFacet.tsx
@@ -52,16 +52,16 @@ class RangeFacet extends React.PureComponent<PropsType, State> {
         }
 
         let onLowerChange = (e) => {
-            let lower = Date.parse(e.target.value + " GMT-0800"); // adding timezones to deal with auto offsetting
+            let lower = Date.parse((e.target.value || e)+ " GMT-0800"); // adding timezones to deal with auto offsetting
             let upper = Date.parse(upperValue + " GMT-0800");
-            lowerValue = e.target.value;
+            lowerValue = e.target.value || e;
             onRangeChange(new Date(lower), new Date(upper));
             afterRangeChange();
         };
         let onUpperChange = (e) => {
-            let upper = Date.parse(e.target.value  + " GMT-0800");
+            let upper = Date.parse((e.target.value || e) + " GMT-0800");
             let lower = Date.parse(lowerValue  + " GMT-0800");
-            upperValue = e.target.value;
+            upperValue = e.target.value || e;
             onRangeChange(new Date(lower), new Date(upper));
             afterRangeChange();
         };
